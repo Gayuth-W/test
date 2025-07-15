b7e1fb98d6ea012c414f23724698283b28c65e0f
Gayuth-W <gayuthdemika@gmail.com>
2025-07-15 20:45:37 +0530

    Delete test/src directory

--- test/src/clickServlet.java
+++
@@ -1,9 +0,0 @@
-// Source code is decompiled from a .class file using FernFlower decompiler.
-public class clickServlet {
-   public clickServlet() {
-   }
-
-   public static void main(String[] var0) {
-      System.out.println("Hello, Wdfdforld!");
-   }
-}
